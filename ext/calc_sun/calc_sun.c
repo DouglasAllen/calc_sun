e90b57480266d7b2b02e1273e58239fdebc56b3a
Douglas <kb9agt@gmail.com>
2017-01-08 22:01:15 -0600

    all parameters use date now all tests pass

--- ext/calc_sun/calc_sun.c
+++ ext/calc_sun/calc_sun.c
@@ -13,7 +13,7 @@
 # define M2PI M_PI * 2.0
 # define INV24 1.0 / 24.0
 # define INV360 1.0 / 360.0
-# define DJ00 2451545.0
+# define DJ00 2451545.0L
 
 
 #define FLOOR(a) lrintf(floorf(a))
@@ -50,21 +50,17 @@ static VALUE t_init(VALUE self){
   return self;
 }
 
-static VALUE func_get_jd(VALUE self, VALUE vdate_time){
-  int year = NUM2INT(rb_funcall(vdate_time, rb_intern("year"), 0));
-  int month = NUM2INT(rb_funcall(vdate_time, rb_intern("month"), 0));
-  int day = NUM2INT(rb_funcall(vdate_time, rb_intern("day"), 0));
+static VALUE func_get_jd(VALUE self, VALUE vdate){
+  int year = NUM2INT(rb_funcall(vdate, rb_intern("year"), 0));
+  int month = NUM2INT(rb_funcall(vdate, rb_intern("month"), 0));
+  int day = NUM2INT(rb_funcall(vdate, rb_intern("day"), 0));
   double jd = civil_to_jd(year, month, day, ITALY) - 13;
   return DBL2NUM(jd);
 }
 
-static VALUE func_rev12(VALUE self, VALUE vx){
-  double x = NUM2DBL(vx);
-  return DBL2NUM(x - 24.0 * floor(x * INV24 + 0.5));
-}
 
 static VALUE func_mean_anomaly(VALUE self, VALUE vdate){
-  double jd = func_get_jd(self, vdate);
+  double jd = NUM2DBL(func_get_jd(self, vdate));
   double d = jd - DJ00;
   double vma =
   fmod(
@@ -74,19 +70,20 @@ static VALUE func_mean_anomaly(VALUE self, VALUE vdate){
   return DBL2NUM(vma);
 }
 
-static VALUE func_eccentricity(VALUE self, VALUE vd){
-  double d = NUM2DBL(vd);
+static VALUE func_eccentricity(VALUE self, VALUE vdate){
+  double jd = NUM2DBL(func_get_jd(self, vdate));
+  double d = jd - DJ00;
   double ve =
   0.016709 -
   1.151e-9 * d;
   return DBL2NUM(ve);
 }
 
-static VALUE func_equation_of_center(VALUE self, VALUE vd){
+static VALUE func_equation_of_center(VALUE self, VALUE vdate){
   double vma =
-  NUM2DBL(func_mean_anomaly(self, vd));
+  NUM2DBL(func_mean_anomaly(self, vdate));
   double ve =
-  NUM2DBL(func_eccentricity(self, vd));
+  NUM2DBL(func_eccentricity(self, vdate));
   double ve2 = ve * 2.0;
   double vesqr = ve * ve;
   double vesqr54 = 5.0 / 4.0 * vesqr;
@@ -98,17 +95,18 @@ static VALUE func_equation_of_center(VALUE self, VALUE vd){
   return DBL2NUM(veoc);
 }
 
-static VALUE func_true_anomaly(VALUE self, VALUE vd){
+static VALUE func_true_anomaly(VALUE self, VALUE vdate){
   double vma =
-  NUM2DBL(func_mean_anomaly(self, vd));
+  NUM2DBL(func_mean_anomaly(self, vdate));
   double veoc =
-  NUM2DBL(func_equation_of_center(self, vd));
+  NUM2DBL(func_equation_of_center(self, vdate));
   double vta = vma + veoc;
   return DBL2NUM(vta);
 }
 
-static VALUE func_mean_longitude(VALUE self, VALUE vd){
-  double d = NUM2DBL(vd);
+static VALUE func_mean_longitude(VALUE self, VALUE vdate){
+  double jd = NUM2DBL(func_get_jd(self, vdate));
+  double d = jd - DJ00;
   double vml =
   fmod(
     (280.4664567 +
@@ -117,25 +115,27 @@ static VALUE func_mean_longitude(VALUE self, VALUE vd){
   return DBL2NUM(vml);
 }
 
-static VALUE func_eccentric_anomaly(VALUE self, VALUE vd){
+static VALUE func_eccentric_anomaly(VALUE self, VALUE vdate){
   double ve =
-  NUM2DBL(func_eccentricity(self, vd));
+  NUM2DBL(func_eccentricity(self, vdate));
   double vml =
-  NUM2DBL(func_mean_longitude(self, vd));
+  NUM2DBL(func_mean_longitude(self, vdate));
   double vea =
   vml + ve * sin(vml) * (1.0 + ve * cos(vml));
   return DBL2NUM(vea);
 }
 
-static VALUE func_obliquity_of_ecliptic(VALUE self, VALUE vd){
-  double d = NUM2DBL(vd);
+static VALUE func_obliquity_of_ecliptic(VALUE self, VALUE vdate){
+  double jd = NUM2DBL(func_get_jd(self, vdate));
+  double d = jd - DJ00;
   double vooe =
   (23.439291 - 3.563E-7 * d) * D2R;
   return DBL2NUM(vooe);
 }
 
-static VALUE func_longitude_of_perihelion(VALUE self, VALUE vd){
-  double d = NUM2DBL(vd);
+static VALUE func_longitude_of_perihelion(VALUE self, VALUE vdate){
+  double jd = NUM2DBL(func_get_jd(self, vdate));
+  double d = jd - DJ00;
   double vlop =
   fmod(
     (282.9404 +
@@ -144,112 +144,113 @@ static VALUE func_longitude_of_perihelion(VALUE self, VALUE vd){
   return DBL2NUM(vlop);
 }
 
-static VALUE func_xv(VALUE self, VALUE vd){
+static VALUE func_xv(VALUE self, VALUE vdate){
   double vea =
-  NUM2DBL(func_eccentric_anomaly(self, vd));
+  NUM2DBL(func_eccentric_anomaly(self, vdate));
   double ve =
-  NUM2DBL(func_eccentricity(self, vd));
+  NUM2DBL(func_eccentricity(self, vdate));
   double vxv = cos(vea) - ve;
   return DBL2NUM(vxv);
 }
 
-static VALUE func_yv(VALUE self, VALUE vd){
+static VALUE func_yv(VALUE self, VALUE vdate){
   double vea =
-  NUM2DBL(func_eccentric_anomaly(self, vd));
+  NUM2DBL(func_eccentric_anomaly(self, vdate));
   double ve =
-  NUM2DBL(func_eccentricity(self, vd));
+  NUM2DBL(func_eccentricity(self, vdate));
   double vyv =
   sqrt(1.0 - ve * ve) * sin(vea);
   return DBL2NUM(vyv);
 }
 
-static VALUE func_true_longitude(VALUE self, VALUE vd){
+static VALUE func_true_longitude(VALUE self, VALUE vdate){
   double vta =
-  NUM2DBL(func_true_anomaly(self, vd));
+  NUM2DBL(func_true_anomaly(self, vdate));
   double vlop =
-  NUM2DBL(func_longitude_of_perihelion(self, vd));
+  NUM2DBL(func_longitude_of_perihelion(self, vdate));
   double vtl =
   fmod(vta + vlop, M2PI);
   return DBL2NUM(vtl);
 }
 
-static VALUE func_rv(VALUE self, VALUE vd){
+static VALUE func_rv(VALUE self, VALUE vdate){
   double vxv =
-  NUM2DBL(func_xv(self, vd));
+  NUM2DBL(func_xv(self, vdate));
   double vyv =
-  NUM2DBL(func_yv(self, vd));
+  NUM2DBL(func_yv(self, vdate));
   double vrv =
   sqrt(vxv * vxv + vyv * vyv);
   return DBL2NUM(vrv);
 }
 
-static VALUE func_ecliptic_x(VALUE self, VALUE vd){
+static VALUE func_ecliptic_x(VALUE self, VALUE vdate){
   double vrv =
-  NUM2DBL(func_rv(self, vd));
+  NUM2DBL(func_rv(self, vdate));
   double vtl =
-  NUM2DBL(func_true_longitude(self, vd));
+  NUM2DBL(func_true_longitude(self, vdate));
   double vex = vrv * cos(vtl);
   return DBL2NUM(vex);
 }
 
-static VALUE func_ecliptic_y(VALUE self, VALUE vd){
+static VALUE func_ecliptic_y(VALUE self, VALUE vdate){
   double vrv =
-  NUM2DBL(func_rv(self, vd));
+  NUM2DBL(func_rv(self, vdate));
   double vtl =
-  NUM2DBL(func_true_longitude(self, vd));
+  NUM2DBL(func_true_longitude(self, vdate));
   double vey = vrv * sin(vtl);
   return DBL2NUM(vey);
 }
 
-static VALUE func_right_ascension(VALUE self, VALUE vd){
+static VALUE func_right_ascension(VALUE self, VALUE vdate){
   double vey =
-  NUM2DBL(func_ecliptic_y(self, vd));
+  NUM2DBL(func_ecliptic_y(self, vdate));
   double vooe =
-  NUM2DBL(func_obliquity_of_ecliptic(self, vd));
+  NUM2DBL(func_obliquity_of_ecliptic(self, vdate));
   double vex =
-  NUM2DBL(func_ecliptic_x(self, vd));
+  NUM2DBL(func_ecliptic_x(self, vdate));
   double vra =
   fmod(atan2(vey * cos(vooe), vex) + M2PI, M2PI);
   return DBL2NUM(vra * R2D / 15.0);
 }
 
-static VALUE func_declination(VALUE self, VALUE vd){
+static VALUE func_declination(VALUE self, VALUE vdate){
   double vex =
-  NUM2DBL(func_ecliptic_x(self, vd));
+  NUM2DBL(func_ecliptic_x(self, vdate));
   double vey =
-  NUM2DBL(func_ecliptic_y(self, vd));
+  NUM2DBL(func_ecliptic_y(self, vdate));
   double vooe =
-  NUM2DBL(func_obliquity_of_ecliptic(self, vd));
+  NUM2DBL(func_obliquity_of_ecliptic(self, vdate));
   double ver = sqrt(vex * vex + vey * vey);
   double vz = vey * sin(vooe);
   double vdec = atan2(vz, ver);
   return DBL2NUM(vdec);
 }
 
-static VALUE func_sidetime(VALUE self, VALUE vjd){
-  double vd = NUM2DBL(vjd);
+static VALUE func_sidetime(VALUE self, VALUE vdate){
+  double jd = NUM2DBL(func_get_jd(self, vdate));
+  double d = jd - DJ00;
   double vst =
   fmod(
     (180 + 357.52911 + 282.9404) +
-    (0.985600281725 + 4.70935E-5) * vd, 360.0);
+    (0.985600281725 + 4.70935E-5) * d, 360.0);
   return DBL2NUM(vst / 15.0);
 }
 
-static VALUE func_local_sidetime(VALUE self, VALUE vjd, VALUE vlon){
-  double vst = NUM2DBL(func_sidetime(self, vjd));
+static VALUE func_local_sidetime(VALUE self, VALUE vdate, VALUE vlon){
+  double vst = NUM2DBL(func_sidetime(self, vdate));
   double vlst = NUM2DBL(vlon) / 15.0 + 12.0 + vst;
   return DBL2NUM(fmod(vlst, 24.0));
 }
 
-static VALUE func_dlt(VALUE self, VALUE vd, VALUE vlat){
+static VALUE func_dlt(VALUE self, VALUE vdate, VALUE vlat){
   double vsin_alt = sin(-0.8333 * D2R);
   double vlat_r = NUM2DBL(vlat) * D2R;
   double vcos_lat = cos(vlat_r);
   double vsin_lat = sin(vlat_r);
   double vooe =
-  NUM2DBL(func_obliquity_of_ecliptic(self, vd));
+  NUM2DBL(func_obliquity_of_ecliptic(self, vdate));
   double vtl =
-  NUM2DBL(func_true_longitude(self, vd));
+  NUM2DBL(func_true_longitude(self, vdate));
   double vsin_dec = sin(vooe) * sin(vtl);
   double vcos_dec =
   sqrt( 1.0 - vsin_dec * vsin_dec );
@@ -262,53 +263,53 @@ static VALUE func_dlt(VALUE self, VALUE vd, VALUE vlat){
   return DBL2NUM(vdlt);
 }
 
-static VALUE func_diurnal_arc(VALUE self, VALUE vjd, VALUE vlat){
-  double dlt = NUM2DBL(func_dlt(self, vjd, vlat));
+static VALUE func_diurnal_arc(VALUE self, VALUE vdate, VALUE vlat){
+  double dlt = NUM2DBL(func_dlt(self, vdate, vlat));
   double da = dlt / 2.0;
   return DBL2NUM(da);
 }
 
-static VALUE func_t_south(VALUE self, VALUE vjd, VALUE vlon){
-  double lst = NUM2DBL(func_local_sidetime(self, vjd, vlon));
-  double ra = NUM2DBL(func_right_ascension(self, vjd));
+static VALUE func_t_south(VALUE self, VALUE vdate, VALUE vlon){
+  double lst = NUM2DBL(func_local_sidetime(self, vdate, vlon));
+  double ra = NUM2DBL(func_right_ascension(self, vdate));
   double vx = lst - ra;
   double vt = vx - 24.0 * floor(vx * INV24 + 0.5);
   return DBL2NUM(12 - vt);
 }
 
-static VALUE func_t_rise(VALUE self, VALUE vjd,  VALUE vlat, VALUE vlon){
-  double ts = NUM2DBL(func_t_south(self, vjd, vlon));
-  double da = NUM2DBL(func_diurnal_arc(self, vjd, vlat));
+static VALUE func_t_rise(VALUE self, VALUE vdate,  VALUE vlat, VALUE vlon){
+  double ts = NUM2DBL(func_t_south(self, vdate, vlon));
+  double da = NUM2DBL(func_diurnal_arc(self, vdate, vlat));
   return DBL2NUM(ts - da);
 }
 
-static VALUE func_t_mid_day(VALUE self, VALUE vjd, VALUE vlat, VALUE vlon){
-  double ts = NUM2DBL(func_t_south(self, vjd, vlon));
+static VALUE func_t_mid_day(VALUE self, VALUE vdate, VALUE vlat, VALUE vlon){
+  double ts = NUM2DBL(func_t_south(self, vdate, vlon));
   return DBL2NUM(ts);
 }
 
-static VALUE func_t_set(VALUE self, VALUE vjd, VALUE vlat, VALUE vlon){
-  double ts = NUM2DBL(func_t_south(self, vjd, vlon));
-  double da = NUM2DBL(func_diurnal_arc(self, vjd, vlat));
+static VALUE func_t_set(VALUE self, VALUE vdate, VALUE vlat, VALUE vlon){
+  double ts = NUM2DBL(func_t_south(self, vdate, vlon));
+  double da = NUM2DBL(func_diurnal_arc(self, vdate, vlat));
   return DBL2NUM(ts + da);
 }
 
-static VALUE func_rise(VALUE self, VALUE vjd, VALUE vlat, VALUE vlon){
-  double rt = NUM2DBL(func_t_rise(self, vjd, vlat, vlon));
+static VALUE func_rise(VALUE self, VALUE vdate, VALUE vlat, VALUE vlon){
+  double rt = NUM2DBL(func_t_rise(self, vdate, vlat, vlon));
   printf("\tSun rises \t\t\t : %2.0f:%02.0f \n",
   floor(rt), floor(fmod(rt, 1) * 60.0));
   return Qnil;
 }
 
-static VALUE func_noon(VALUE self, VALUE vjd, VALUE vlat, VALUE vlon){
-  double nt = NUM2DBL(func_t_south(self, vjd, vlon));
+static VALUE func_noon(VALUE self, VALUE vdate, VALUE vlat, VALUE vlon){
+  double nt = NUM2DBL(func_t_south(self, vdate, vlon));
   printf("\tSun midday \t\t\t : %2.0f:%02.0f \n",
   floor(nt), floor(fmod(nt, 1) * 60.0));
   return Qnil;
 }
 
-static VALUE func_set(VALUE self, VALUE vjd, VALUE vlat, VALUE vlon){
-  double st = NUM2DBL(func_t_set(self, vjd, vlat, vlon));
+static VALUE func_set(VALUE self, VALUE vdate, VALUE vlat, VALUE vlon){
+  double st = NUM2DBL(func_t_set(self, vdate, vlat, vlon));
   printf("\tSun sets \t\t\t : %2.0f:%02.0f \n",
   floor(st), floor(fmod(st, 1) * 60.0));
   return Qnil;
@@ -322,19 +323,24 @@ GetDaysTillJ2000(int year, int month, int day, double hours){
      return(days_till_J2000);
 }
 
-static VALUE func_jd_from_2000(VALUE self, VALUE vdate_time){
-  int year = NUM2INT(rb_funcall(vdate_time, rb_intern("year"), 0));
-  int month = NUM2INT(rb_funcall(vdate_time, rb_intern("month"), 0));
-  int day = NUM2INT(rb_funcall(vdate_time, rb_intern("day"), 0));
+static VALUE func_jd_from_2000(VALUE self, VALUE vdate){
+  int year = NUM2INT(rb_funcall(vdate, rb_intern("year"), 0));
+  int month = NUM2INT(rb_funcall(vdate, rb_intern("month"), 0));
+  int day = NUM2INT(rb_funcall(vdate, rb_intern("day"), 0));
   double days = GetDaysTillJ2000(year, month, day, 12);
   return DBL2NUM(days);
 }
 
-static VALUE func_days_from_2000(VALUE self, VALUE vdate_time, VALUE vlon){
-  double jd = NUM2DBL(func_get_jd(self, vdate_time));
+static VALUE func_days_from_2000(VALUE self, VALUE vdate, VALUE vlon){
+  double jd = NUM2DBL(func_get_jd(self, vdate));
   double lon = NUM2DBL(vlon);
   double days = jd - DJ00 - lon / 360;
   return DBL2NUM(days);
+}
+
+static VALUE func_rev12(VALUE self, VALUE vx){
+  double x = NUM2DBL(vx);
+  return DBL2NUM(x - 24.0 * floor(x * INV24 + 0.5));
 }
 
 VALUE rb_time;
--- test/calc_sun/test_calc_sun.rb
+++ test/calc_sun/test_calc_sun.rb
@@ -19,93 +19,100 @@ require 'date'
 class TestCalcSun100 < Test::Unit::TestCase # MiniTest::Test
   def setup
     @t = CalcSun.new
-    @t_date = Date.parse('2017-01-03')
+    @t_date = Date.parse('2000-01-01')
     @t_ajd = 0.0
     @t_lat = 0.0
     @t_lon = 0.0
   end
 
+  def test_jd
+    assert_equal(
+      2_451_545.0,
+      @t.jd(@t_date).round(12)
+    )
+  end
+
   def test_mean_anomaly
     assert_equal(
       6.240059966692,
-      @t.mean_anomaly().round(12)
+      @t.mean_anomaly(@t_date).round(12)
     )
   end
 
   def test_eccentricity
     assert_equal(
       0.016709,
-      @t.eccentricity(@t_ajd)
+      @t.eccentricity(@t_date)
     )
   end
 
   def test_equation_of_center
     assert_equal(
       -0.001471380867,
-      @t.equation_of_center(@t_ajd).round(12)
+      @t.equation_of_center(@t_date).round(12)
     )
   end
 
   def test_true_anomaly
     assert_equal(
       6.238588585825,
-      @t.true_anomaly(@t_ajd).round(12)
+      @t.true_anomaly(@t_date).round(12)
     )
   end
 
   def test_mean_longitude
     assert_equal(
       4.895063110817,
-      @t.mean_longitude(@t_ajd).round(12)
+      @t.mean_longitude(@t_date).round(12)
     )
   end
 
   def test_eccentric_anomaly
     assert_equal(
       4.878582250862,
-      @t.eccentric_anomaly(@t_ajd).round(12)
+      @t.eccentric_anomaly(@t_date).round(12)
     )
   end
 
   def test_obliquity_of_ecliptic
     assert_equal(
       0.409092802283,
-      @t.obliquity_of_ecliptic(@t_ajd).round(12)
+      @t.obliquity_of_ecliptic(@t_date).round(12)
     )
   end
 
   def test_xv
     assert_equal(
       0.148720277673,
-      @t.xv(@t_ajd).round(12)
+      @t.xv(@t_date).round(12)
     )
   end
 
   def test_yv
     assert_equal(
       -0.986083974099,
-      @t.yv(@t_ajd).round(12)
+      @t.yv(@t_date).round(12)
     )
   end
 
   def test_longitude_of_perihelion
     assert_equal(
       4.93824156691,
-      @t.longitude_of_perihelion(@t_ajd).round(12)
+      @t.longitude_of_perihelion(@t_date).round(12)
     )
   end
 
   def test_true_longitude
     assert_equal(
       4.893644845555,
-      @t.true_longitude(@t_ajd).round(12)
+      @t.true_longitude(@t_date).round(12)
     )
   end
 
   def test_radius_vector
     assert_equal(
       0.997235842199,
-      @t.radius_vector(@t_ajd).round(12)
+      @t.radius_vector(@t_date).round(12)
     )
   end
 end
@@ -114,115 +121,115 @@ end
 class TestCalcSun200 < Test::Unit::TestCase # MiniTest::Test
   def setup
     @t = CalcSun.new
-    @t_ajd = 0.0
+    @t_date = Date.parse('2017-01-03')
     @t_lat = 0.0
     @t_lon = 0.0
   end
 
   def test_ecliptic_x
     assert_equal(
-      0.17976672602,
-      @t.ecliptic_x(@t_ajd).round(12)
+      0.229457387793,
+      @t.ecliptic_x(@t_date).round(12)
     )
   end
 
   def test_ecliptic_y
     assert_equal(
-      -0.980899204395,
-      @t.ecliptic_y(@t_ajd).round(12)
+      -0.96964143837,
+      @t.ecliptic_y(@t_date).round(12)
     )
   end
 
   def test_right_ascension
     assert_equal(
-      18.753078192426,
-      @t.right_ascension(@t_ajd).round(12)
+      18.964170843151,
+      @t.right_ascension(@t_date).round(12)
     )
   end
 
   def test_declination
     assert_equal(
-      -0.372949956542,
-      @t.declination(@t_ajd).round(12)
+      -0.369294740577,
+      @t.declination(@t_date).round(12)
     )
   end
 
   def test_sidereal_time
     assert_equal(
-      6.697967333333,
-      @t.sidereal_time(@t_ajd).round(12)
+      6.887400326513,
+      @t.sidereal_time(@t_date).round(12)
     )
   end
 
   def test_local_sidereal_time
     assert_equal(
-      18.697967333333,
-      @t.local_sidereal_time(@t_ajd, @t_lon).round(12)
+      18.887400326513,
+      @t.local_sidereal_time(@t_date, @t_lon).round(12)
     )
   end
 
   def test_daylight_time
     assert_equal(
-      12.120732161881,
-      @t.daylight_time(@t_ajd, @t_lat).round(12)
+      12.120499330542,
+      @t.daylight_time(@t_date, @t_lat).round(12)
     )
   end
 
   def test_diurnal_arc
     assert_equal(
-      6.06036608094,
-      @t.diurnal_arc(@t_ajd, @t_lat).round(12)
+      6.060249665271,
+      @t.diurnal_arc(@t_date, @t_lat).round(12)
     )
   end
 
   def test_t_south
     assert_equal(
-      12.055110859092,
-      @t.t_south(@t_ajd, @t_lon).round(12)
+      12.076770516638,
+      @t.t_south(@t_date, @t_lon).round(12)
     )
   end
 
   def test_t_rise
     assert_equal(
-      5.994744778152,
-      @t.t_rise(@t_ajd, @t_lon, @t_lat).round(12)
+      6.016520851367,
+      @t.t_rise(@t_date, @t_lon, @t_lat).round(12)
     )
   end
 
   def test_t_mid_day
     assert_equal(
-      12.055110859092,
-      @t.t_mid_day(@t_ajd, @t_lat, @t_lon).round(12)
+      12.076770516638,
+      @t.t_mid_day(@t_date, @t_lat, @t_lon).round(12)
     )
   end
 
   def test_t_set
     assert_equal(
-      18.115476940033,
-      @t.t_set(@t_ajd, @t_lon, @t_lat).round(12)
+      18.137020181909,
+      @t.t_set(@t_date, @t_lon, @t_lat).round(12)
     )
   end
 
   def test_rise_time
-    rise = @t.t_rise(@t_ajd, @t_lon, @t_lat).round(12)
+    rise = @t.t_rise(@t_date, @t_lon, @t_lat).round(12)
     assert_equal(
-      "Sun rises \t\t\t : 5:59 UTC",
+      "Sun rises \t\t\t : 6:0 UTC",
       "Sun rises \t\t\t : #{rise.floor}:#{(rise % 1 * 60.0).floor} UTC"
     )
   end
 
   def test_midday_time
-    midday = @t.t_mid_day(@t_ajd, @t_lat, @t_lon).round(12)
+    midday = @t.t_mid_day(@t_date, @t_lat, @t_lon).round(12)
     assert_equal(
-      "Sun mid day \t\t\t : 12:3 UTC",
+      "Sun mid day \t\t\t : 12:4 UTC",
       "Sun mid day \t\t\t : #{midday.floor}:#{((midday % 1.0) * 60).floor} UTC"
     )
   end
 
   def test_set_time
-    set = @t.t_set(@t_ajd, @t_lon, @t_lat).round(12)
+    set = @t.t_set(@t_date, @t_lon, @t_lat).round(12)
     assert_equal(
-      "Sun sets \t\t\t : 18:6 UTC",
+      "Sun sets \t\t\t : 18:8 UTC",
       "Sun sets \t\t\t : #{set.floor}:#{(set % 1 * 60.0).floor} UTC"
     )
   end
@@ -232,7 +239,7 @@ end
 class TestCalcSun300 < Test::Unit::TestCase # MiniTest::Test
   def setup
     @t = CalcSun.new
-    @t_ajd = 0.0
+    @t_date = 0.0
     @t_lat = 0.0
     @t_lon = 0.0
   end
@@ -252,3 +259,5 @@ class TestCalcSun300 < Test::Unit::TestCase # MiniTest::Test
     assert_equal(dt_obj.jd - CalcSun::DJ00, @t.jd2000_dif_lon(dt_obj, @t_lon))
   end
 end
+
+
